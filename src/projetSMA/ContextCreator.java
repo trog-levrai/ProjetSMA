ee00baad6fa67dba4d48f8f5941943675392bd62
Ilan Dubois <ilan.dubois@epita.fr>
2017-06-07 19:43:19 +0200

    Add big map

--- src/projetSMA/ContextCreator.java
+++ src/projetSMA/ContextCreator.java
@@ -36,7 +36,7 @@ public class ContextCreator implements ContextBuilder<Object> {
 	public Context<Object> build(Context<Object> context) {
 		context.setId("ProjetSMA");
 		int nb = 100;
-		String path = "tests/foo.sma";
+		String path = "tests/big.map";
 
 		try {
 			Stream<String> stream = Files.lines(Paths.get(path));
